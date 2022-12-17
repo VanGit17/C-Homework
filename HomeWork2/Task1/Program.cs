86467b76a3eed6a4c0ff39a9d6aae1d9366bd196
Van <bigfilessender@mail.ru>
2022-12-17 12:33:40 +0300

    Решена первая задача

---
+++ HomeWork2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное целое число: ");
+int number = int.Parse(Console.ReadLine());
+if (number<0) number = number*(-1);//Условие для корректной работы программы с отрицательными числами
+if (number>99&&number<1000)
+{
+   int a = (number/10)%10; 
+   
+   Console.Write("Вторая цифра числа: " + a);
+ 
+ }
+else
+Console.WriteLine("извините, но это не трехзначное число, мы так не работаем! ");
+
+
