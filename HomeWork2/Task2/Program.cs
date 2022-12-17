3e32d631ceb27f62b34c492634a9d839111b9c2c
Van <bigfilessender@mail.ru>
2022-12-17 12:50:39 +0300

    решена задча 2

---
+++ HomeWork2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите любое целое число, а мы покажем его третью цифру: ");
+Int64 number = Int64.Parse(Console.ReadLine());
+if (number<0) number = number*(-1);//Условие для корректной работы программы с отрицательными числами
+if (number>99)
+{
+   while (number>1000)
+                       {
+                           number = number/10;
+                       }
+   
+   Console.Write("Третья цифра числа: " + number%10);
+  }
+else
+Console.WriteLine("извините, но у этого числа третьей цифры нет! ");
