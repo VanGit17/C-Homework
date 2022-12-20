146b33a4d8ff8c8961da7125c2f9692c36006fad
Van <bigfilessender@mail.ru>
2022-12-20 23:47:48 +0300

    решена задача 2

---
+++ HomeWork4/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+Console.WriteLine("Введите число: ");
+Int64 number = Int64.Parse(Console.ReadLine());
+Int64 sum = 0;
+if (number<0) number=number*(-1);
+while (number>=10)
+{
+    sum =sum+number%10;
+    number = number/10; 
+}
+sum = sum + number;
+    Console.WriteLine("Сумма цифр в числе: "+sum);
