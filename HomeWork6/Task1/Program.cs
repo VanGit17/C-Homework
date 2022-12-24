8f2eb50b67948d4436c60dd7fe1626ce499ca666
Van <bigfilessender@mail.ru>
2022-12-24 20:46:32 +0300

    Решена первая задача

---
+++ HomeWork6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Пример:
+0, 7, 8, -2, -2 -> 2*/
+Console.WriteLine("введите ряд чисел, через пробел, а мы посчитаем сколько из них больше нуля:");
+string row = Console.ReadLine();
+string[] numbers = row.Split(' ');
+int count =0;
+
+for(int i = 0; i<numbers.Length; i++)
+{
+    if(Convert.ToInt16(numbers[i])>0) count +=1;
+}
+ 
+ Console.WriteLine("Положительных чисел в ряду: " + count);
